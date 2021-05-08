56de758a33b550d0158aeef177310a3baf850f4a
Emil Karlsson <own.emka@gmail.com>
2021-05-08 13:12:00 +0200

    Add monopp api

--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,29 @@
 project "mono"
-    kind "None"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
 	
     files
     {
-        "include/mono/**.*",
+        "include/monopp/**.*",
     }
 
     includedirs
     {
         "include"
     }
+    
+    filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
